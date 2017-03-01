9535e6e7da73b1bd7d77ea715dd37133ba1d6992
Janis Kirsteins <janis.kirsteins@notakey.com>
2017-03-01 11:17:15 +0200

    Register.bat/unregister.bat fixes
    - these files can now be invoked from explorer
      as admin (they set the correct working dir)
      - added pause
      - fixed service uninstall/stop order

--- unregister.bat
+++ unregister.bat
@@ -1,4 +1,9 @@
-NotakeyBGService\winsw.exe uninstall
+@setlocal enableextensions
+@cd /d "%~dp0"
+
 NotakeyBGService\winsw.exe stop
+NotakeyBGService\winsw.exe uninstall
 "%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe" /unregister /codebase "./NotakeyNETProvider/bin/x64/Debug/NotakeyNETProvider.dll"
-reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\Credential Providers\{77E5F42E-B280-4219-B130-D48BB3932A04}"
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\Credential Providers\{77E5F42E-B280-4219-B130-D48BB3932A04}" /reg:64
+
+pause
