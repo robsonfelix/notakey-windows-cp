47c6ea6a9377175f17929bbc7909f7509566d0d4
Janis Kirsteins <janis.kirsteins@notakey.com>
2017-03-03 17:54:27 +0200

    Updated product versioning in appveyor.yml

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Notakey Latvia")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+
+// These values should be set 
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
