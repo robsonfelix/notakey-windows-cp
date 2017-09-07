a02a18a684eeb8cf6c2638c3e0eb5111206bd507
Ainārs Kumpiņš <ainars.kumpins@gmail.com>
2017-09-07 17:28:30 +0300

    Version bump from 1.0.86 to 1.0.87

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 
 // These values should be set 
-[assembly: AssemblyVersion("1.0.86.0")]
-[assembly: AssemblyFileVersion("1.0.86.0")]
+[assembly: AssemblyVersion("1.0.87.0")]
+[assembly: AssemblyFileVersion("1.0.87.0")]
