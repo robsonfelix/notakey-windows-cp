765ea151177de3c9fdd5321d1560cecfbf17540f
Ainārs Kumpiņš <ainars.kumpins@gmail.com>
2017-09-07 14:12:13 +0300

    register, unregister dll path

--- register.bat
+++ register.bat
@@ -1,7 +1,7 @@
 @setlocal enableextensions
 @cd /d "%~dp0"
 
-"%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe" /register /codebase "./NotakeyNETProvider/bin/x64/Debug/NotakeyNETProvider.dll"
+"%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe" /register /codebase "./NotakeyNETProvider/bin/x64/Release/NotakeyNETProvider.dll"
 reg import register.reg /reg:64
 NotakeyBGService\winsw.exe install
 NotakeyBGService\winsw.exe start
--- unregister.bat
+++ unregister.bat
@@ -3,7 +3,7 @@
 
 NotakeyBGService\winsw.exe stop
 NotakeyBGService\winsw.exe uninstall
-"%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe" /unregister /codebase "./NotakeyNETProvider/bin/x64/Debug/NotakeyNETProvider.dll"
+"%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe" /unregister /codebase "./NotakeyNETProvider/bin/x64/Release/NotakeyNETProvider.dll"
 reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\Credential Providers\{77E5F42E-B280-4219-B130-D48BB3932A04}" /reg:64
 
 pause
