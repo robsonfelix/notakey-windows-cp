05d281acb75a2700688bc2a467eb9aed0ab90dc5
Ainārs Kumpiņš <ainars.kumpins@gmail.com>
2017-09-08 14:04:41 +0300

    Version bump from 1.0.98 to 1.0.99

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 
 // These values should be set 
-[assembly: AssemblyVersion("1.0.98.0")]
-[assembly: AssemblyFileVersion("1.0.98.0")]
+[assembly: AssemblyVersion("1.0.99.0")]
+[assembly: AssemblyFileVersion("1.0.99.0")]
