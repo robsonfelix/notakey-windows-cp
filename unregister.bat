9e4053a6cd31c97be7e8280393945aeb22db3793
Janis Kirsteins <janis.kirsteins@notakey.com>
2017-09-28 16:57:55 +0300

    Updated unregister* bat files to delete the root/clsid entry for the CP
    regasm /unregister does not remove it, and it might
    have caused stale versions of the dll to become registered

--- unregister.bat
+++ unregister.bat
@@ -4,6 +4,7 @@
 NotakeyBGService\winsw.exe stop
 NotakeyBGService\winsw.exe uninstall
 "%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe" /unregister /codebase "./NotakeyNETProvider/bin/x64/Debug/NotakeyNETProvider.dll"
+reg delete "HKEY_CLASSES_ROOT\CLSID\{77E5F42E-B280-4219-B130-D48BB3932A04}" /reg:64
 reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\Credential Providers\{77E5F42E-B280-4219-B130-D48BB3932A04}" /reg:64
 
 pause
