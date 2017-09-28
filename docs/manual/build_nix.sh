c0925a6bf9b8cf8f490950f0d2fd984419298ee5
Janis Kirsteins <janis.kirsteins@notakey.com>
2017-09-28 17:11:59 +0300

    Build documentation using the correct version

--- docs/manual/build_nix.sh
+++ docs/manual/build_nix.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Use this to get full version
-# VERSION=$(cat ../../GlobalAssemblyInfo.cs | grep AssemblyVe | cut -c 29- | rev | cut -c 4- | rev)
-
-# This drops the last 2 dot-separated values
-VERSION=$(cat ../../GlobalAssemblyInfo.cs | grep AssemblyVe | cut -c 29- | rev | cut -c 4- | rev | sed 's/\.[^.]*\.[^.]*$//')
+# DO NOT USE GlobalAssemblyInfo TO GET VERSION
+#
+# Because the tag determines the version. AppVeyor patches GlobalAssemblyInfo according
+# to the tag.
+#
+VERSION=$(git describe)
 VERSION=$VERSION aiv=$VERSION bundle exec middleman build
 
